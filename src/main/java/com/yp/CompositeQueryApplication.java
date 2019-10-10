e43fd73f7e5c4fcdf46c6f22388cb8f96f8c0102
fengzheng <2581379953@qq.com>
2019-10-10 21:54:17 +0800

    项目初始化提交

---
+++ src/main/java/com/yp/CompositeQueryApplication.java
@@ -0,0 +1,13 @@
+package com.yp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeQueryApplication.class, args);
+	}
+
+}
